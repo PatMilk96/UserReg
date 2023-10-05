aa3b4de5796c520933127c1c648d75630ce3a350
Patryk Milkiewicz <G00397265@atu.ie>
2023-10-05 14:36:59 +0100

    Added Get Mapping for the user controller and created a new user service class

--- src/main/java/ie/atu/userreg/UserController.java
+++ src/main/java/ie/atu/userreg/UserController.java
@@ -1,4 +1,16 @@
 package ie.atu.userreg;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    private UserService us;
+    @GetMapping("/registerUser/{name}/{email}")
+    public UserController register(@PathVariable int name, @PathVariable int email){
+
+        return null;
+    }
 }
