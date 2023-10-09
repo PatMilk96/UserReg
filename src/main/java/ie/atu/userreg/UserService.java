b7d91e9c322d886512a17fe19ef544390deecd87
Patryk Milkiewicz <g00397265@atu.ie>
2023-10-09 22:21:31 +0100

    Added User class and added annotations. Added a PostMapping in the controller with RequestBody

--- src/main/java/ie/atu/userreg/UserService.java
+++ src/main/java/ie/atu/userreg/UserService.java
@@ -1,18 +1,23 @@
 package ie.atu.userreg;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final EmailService emailService;
 
-    @Autowired
     public UserService(EmailService emailService) {
         this.emailService = emailService;
     }
 
+
     public void registerUser(String name, String email){
         emailService.sendEmail(email, "Welcome to our platform!");
     }
+
+    public String confMessage(User user){
+         return "User " + user.getName() + " registered with email: " + user.getEmail();
+    }
+
 }
